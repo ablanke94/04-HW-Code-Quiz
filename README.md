0e6aeaf0af317e11aa49c3d0446a385b9bb6e0aa
Alex Blanke <alexblanke98@gmail.com>
2022-06-12 14:46:01 -0500

    final code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 04-HW-Code-Quiz
+
+Make a multiple choice timed quiz with the ability to use local storage to save high scores.
---
+++ assets/js/questions.js
@@ -0,0 +1,55 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "Javascript is an _______ language?",
+    choices: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the Above"
+    ],
+    answer: "Object-Oriented"
+  },
+  {
+    title: "Which of the following keywords is used to define a variable in Javascript?",
+    choices: [
+      "var",
+      "let",
+      "Both var and let",
+      "None of the Above"
+    ],
+    answer: "Both var and let"
+  },
+  {
+    title: "Which of the following methods is used to access HTML elements using Javascript?",
+    choices: [
+      "getElementbyId()",
+      "getElementsByClassName()",
+      "Both of the above",
+      "None of the above"
+    ],
+    answer: "Both of the above"
+  },
+  {
+    title:
+      "How can a datatype be declared to be a constant type?",
+    choices: [
+      "const",
+      "var",
+      "let",
+      "constant"
+    ],
+    answer: "const"
+  },
+  {
+    title:
+      "What is the use of the <noscript> tag in Javascript?",
+    choices: [
+      "The contents are displayed by non-JS-based browsers.",
+      "Clears all the cookies and cache.",
+      "Both of the above.",
+      "None of the above."
+    ],
+    answer: "The contents are displayed by non-JS-based browsers."
+  }
+];
